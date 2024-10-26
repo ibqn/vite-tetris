4e1eb50cb826f93b189298b44b939c90b1e23c2b
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-10-26 19:52:27 +0200

    improvements

--- src/components/tetris.tsx
+++ src/components/tetris.tsx
@@ -2,18 +2,10 @@ import { Board } from '@/components/board'
 import { UpcomingBlocks } from '@/components/upcoming-blocks'
 import { CurrentScore } from '@/components/current-score'
 import { useTetris } from '@/hooks/use-tetris'
-import { Block } from '@/types'
 import { Button } from '@/components/button'
 
 export const Tetris = () => {
   const { board, upcomingBlocks, startGame } = useTetris()
-
-  board[199] = Block.I
-  board[198] = Block.I
-  board[197] = Block.J
-  board[186] = Block.J
-  board[196] = Block.L
-  board[188] = Block.O
 
   const handleStart = () => {
     startGame()
@@ -26,8 +18,10 @@ export const Tetris = () => {
         <UpcomingBlocks upcomingBlocks={upcomingBlocks} />
         <CurrentScore score={0} />
 
-        <div>
-          <Button onClick={handleStart}>start</Button>
+        <div className="flex">
+          <Button onClick={handleStart} className="flex-1">
+            start
+          </Button>
         </div>
       </div>
     </div>
