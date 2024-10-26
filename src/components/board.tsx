dd1acf957ccc9df4ae13318f195b9442805a80e2
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-10-26 11:44:00 +0200

    pass board as props

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,15 +1,11 @@
-import { Block, BOARD_SIZE } from '@/types'
+import { Block } from '@/types'
 import { Cell } from '@/components/cell'
 
-export const Board = () => {
-  const board = Array.from<Block>({ length: BOARD_SIZE }).fill(Block.EMPTY)
-  board[199] = Block.I
-  board[198] = Block.I
-  board[197] = Block.J
-  board[186] = Block.J
-  board[196] = Block.L
-  board[188] = Block.O
+type Props = {
+  board: Block[]
+}
 
+export const Board = ({ board }: Props) => {
   return (
     <div className="grid grid-cols-10 gap-px border border-white bg-black/70 p-px shadow-glow">
       {board.map((block, index) => (
