51f3a5b937183f6993843571cb727a97517ddc03
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-10-26 19:51:58 +0200

    improve button stylings

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,5 +4,10 @@ import type { ComponentProps } from 'react'
 type Props = ComponentProps<'button'>
 
 export const Button = ({ className, ...props }: Props) => {
-  return <button {...props} className={cn('border p-2', className)}></button>
+  return (
+    <button
+      {...props}
+      className={cn('rounded-lg border border-gray-500 bg-black/70 px-5 py-3 text-sm uppercase', className)}
+    ></button>
+  )
 }
