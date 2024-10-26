23f49e2a1c11d81b7d7f1ed2073f6eb350e72c40
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-10-26 11:44:56 +0200

    swap row with col

--- src/components/upcoming-blocks.tsx
+++ src/components/upcoming-blocks.tsx
@@ -1,5 +1,5 @@
 import { BlockNoEmpty, shapes } from '@/types'
-import { Cell } from './cell'
+import { Cell } from '@/components/cell'
 
 type Props = {
   blocks: BlockNoEmpty[]
@@ -8,13 +8,14 @@ type Props = {
 export const UpcomingBlocks = ({ blocks }: Props) => {
   const [currentBlock] = blocks
   const shape = shapes[currentBlock].shapes[0]
+  console.log('shape', shape)
 
   return (
     <div className="flex max-h-[200px] flex-1 flex-col items-center gap-4 border border-white bg-black/70 px-2 py-4 shadow-glow">
       <div className="uppercase">next</div>
-      <div className="flex flex-row gap-px">
+      <div className="flex flex-col gap-px">
         {shape.map((row, rowIndex) => (
-          <div key={rowIndex} className="flex flex-col gap-px">
+          <div key={rowIndex} className="flex flex-row gap-px">
             {row.map((block, colIndex) => (
               <Cell key={colIndex} block={block} />
             ))}
