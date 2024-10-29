53bb99494d12af7edb7d737eebfe3024e2ff15d1
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-10-29 18:50:22 +0100

    clear lines

---
+++ src/utils/clear-lines.ts
@@ -0,0 +1,13 @@
+import { BOARD_WIDTH, BoardShape, Block } from '@/types'
+import { getEmptyLine } from '@/utils/empty-board'
+
+export const clearLines = (board: BoardShape): BoardShape => {
+  for (let i = 0; i < board.length; i += BOARD_WIDTH) {
+    const row = board.slice(i, i + BOARD_WIDTH)
+    if (row.every((cell) => cell !== Block.EMPTY)) {
+      board.splice(i, BOARD_WIDTH)
+      board.unshift(...getEmptyLine())
+    }
+  }
+  return board
+}
