fd4510fc01a809c6e6f4ff1c41598d4373fba7d1
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-10-29 18:50:08 +0100

    get empty line

--- src/utils/empty-board.ts
+++ src/utils/empty-board.ts
@@ -1,3 +1,5 @@
-import { Block, BOARD_SIZE, BoardShape } from '@/types'
+import { Block, BOARD_SIZE, BOARD_WIDTH, BoardShape } from '@/types'
 
 export const getEmptyBoard = (): BoardShape => Array.from<Block>({ length: BOARD_SIZE }).fill(Block.EMPTY)
+
+export const getEmptyLine = (): BoardShape => Array.from<Block>({ length: BOARD_WIDTH }).fill(Block.EMPTY)
