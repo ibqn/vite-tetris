4074c218f912229d428118250c1504d1dc86f306
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-10-31 13:24:55 +0100

    remove border on focus

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -10,11 +10,7 @@ type Ref = HTMLDivElement
 
 export const Board = forwardRef<Ref, Props>(({ board }, ref) => {
   return (
-    <div
-      ref={ref}
-      tabIndex={-1}
-      className="grid grid-cols-10 gap-px border border-white bg-black/70 p-px shadow-glow focus:border-2"
-    >
+    <div ref={ref} tabIndex={-1} className="grid grid-cols-10 gap-px border border-white bg-black/70 p-px shadow-glow">
       {board.map((block, index) => (
         <Cell key={index} block={block} />
       ))}
